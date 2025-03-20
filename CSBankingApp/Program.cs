6c5b5c7327905a5444783cf15a764d5e97d8b2dc
Ryan Bitrick <rbitrick@gmail.com>
2025-03-19 21:59:40 -0400

    Base loop

--- CSBankingApp/Program.cs
+++ CSBankingApp/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+static void Get_Balance(int balance)
+{
+    Console.WriteLine("****************");
+    Console.WriteLine("Your balance is $" + balance);
+    Console.WriteLine("****************");
+
+}
+
+static void Deposit()
+{
+    Console.WriteLine("****************");
+    Console.WriteLine("Your balance is $" + balance);
+    Console.WriteLine("****************");
+
+}
+
+
+static void Main(string[] args)
+{
+    int balance = 0;
+    bool is_running = true;
+
+    while (is_running)
+    {
+        Console.WriteLine("****************");
+        Console.WriteLine("Welcome to Banking App in C#");
+        Console.WriteLine("****************");
+        Console.WriteLine("1. Get Balance");
+        Console.WriteLine("2. Deposit");
+        Console.WriteLine("3. Withdraw");
+        Console.WriteLine("4. Exit");
+        Console.WriteLine("Please enter an option (1-4): ");
+        string choice = Console.ReadLine();
+
+        if (choice == "1") 
+        {
+            Get_Balance(balance);
+        }
+        else if (choice == "2")
+        {
+            balance += Deposit();
+        }
+
+    }
+}
